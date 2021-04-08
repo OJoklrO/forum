eb063205613b56782c2a39def88b5d202a98228a
Joklr <gxy0082@gmail.com>
2021-04-08 14:00:33 +0800

    finish post api

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"github.com/OJoklrO/forum/global"
+	"github.com/OJoklrO/forum/internal/model"
 	"github.com/OJoklrO/forum/internal/routers"
+	"github.com/OJoklrO/forum/pkg/logger"
+	"github.com/OJoklrO/forum/pkg/setting"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"log"
 	"net/http"
+	"time"
 )
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+	err = setupLogger()
+	if err != nil {
+		log.Fatalf("init.setupLogger err: %v", err)
+	}
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDbEngine err: %v", err)
+	}
+}
 
 // @title forum
 // @version 1.0
@@ -16,4 +37,45 @@ func main() {
 		Handler: router,
 	}
 	log.Fatal(s.ListenAndServe())
+}
+
+func setupSetting() error {
+	s, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	return err
+}
+
+func setupLogger() error {
+	fileName := global.AppSetting.LogSavePath + "/" + global.AppSetting.LogFileName + global.AppSetting.LogFileExt
+	global.Logger = logger.NewLogger(&lumberjack.Logger{
+		Filename: fileName,
+		MaxSize: 600,
+		MaxAge: 10,
+		LocalTime: true,
+	},"", log.LstdFlags).WithCaller(2)
+
+	return nil
 }
--- pkg/errcode/module_code.go
+++ pkg/errcode/module_code.go
@@ -2,10 +2,10 @@ package errcode
 
 var (
 	ErrorCountPostsFail = NewError(20010001, "count posts error")
-	ErrorGetPostFail = NewError(20010002, "get tag list error")
-	ErrorGetPostListFail = NewError(20010003, "count tag error")
-	ErrorCreatePostFail = NewError(20010004, "create tag error")
-	ErrorUpdatePostFail = NewError(20010005, "update tag error")
-	ErrorDeletePostFail = NewError(20010006, "delete tag error")
+	ErrorGetPostFail = NewError(20010002, "get post error")
+	ErrorGetPostListFail = NewError(20010003, "get post list error")
+	ErrorCreatePostFail = NewError(20010004, "create post error")
+	ErrorUpdatePostFail = NewError(20010005, "update post error")
+	ErrorDeletePostFail = NewError(20010006, "delete post error")
 
 )
